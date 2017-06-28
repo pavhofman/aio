a19b979444f9acfc1f67f4451c268d009a4a1038
Pavel Hofman <pavel.hofman@ivitera.com>
2017-06-28 20:39:56 +0200

    WEBAPP - fixing order of containers, optimizing containers replacement

--- uis/mainfscontainer.py
+++ uis/mainfscontainer.py
@@ -1,31 +1,29 @@
 from typing import TYPE_CHECKING
 
 from remi import gui
-
 from uis.overviewpanel import OverviewPanel
+
+TRACK_CONTAINER_KEY = '1'
+OVERVIEW_CONTAINER_KEY = '2'
+
 
 if TYPE_CHECKING:
     from uis.webapp import WebApp
 
 
-class MainFSContainer(gui.Widget):
+class MainFSContainer(gui.HBox):
     def __init__(self, app: 'WebApp'):
         super().__init__(width=app.getWidth(), height=app.getHeight(), margin='0px auto',
                          layout_orientation=gui.Widget.LAYOUT_HORIZONTAL)
         self._app = app
-        self.style['display'] = 'block'
-        self.style['overflow'] = 'hidden'
         self._noTrackContainer = self._createNoSourceTrackContainer()
-        self._trackContainer = self._noTrackContainer
-        self.append(self._trackContainer)
+        self.setNoTrackContainer()
         self._overviewContainer = OverviewPanel(app)
-        self.append(self._overviewContainer)
+        self.append(self._overviewContainer, OVERVIEW_CONTAINER_KEY)
 
     @staticmethod
     def _createNoSourceTrackContainer() -> gui.Widget:
         container = gui.Widget(width=400)
-        container.style['display'] = 'block'
-        container.style['overflow'] = 'auto'
         container.style['text-align'] = 'center'
         container.append(gui.Label(text="No active source"))
         return container
@@ -34,11 +32,11 @@ class MainFSContainer(gui.Widget):
         self._overviewContainer.setVolume(value)
 
     def setTrackContainer(self, container: gui.Widget) -> None:
-        if container is not self._trackContainer:
-            self.empty()
-            self._trackContainer = container
-            self.append(self._trackContainer)
-            self.append(self._overviewContainer)
+        if not self._isCurrentTrackContainer(container):
+            self.append(container, TRACK_CONTAINER_KEY)
+
+    def _isCurrentTrackContainer(self, container: gui.Widget):
+        return TRACK_CONTAINER_KEY in self.children.keys() and container == self.children[TRACK_CONTAINER_KEY]
 
     def setNoTrackContainer(self):
         self.setTrackContainer(self._noTrackContainer)
