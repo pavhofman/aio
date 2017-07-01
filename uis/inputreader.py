51f31b98c78d4d9b35a9bfd3c20d83b2da665fd9
Pavel Hofman <pavel.hofman@ivitera.com>
2017-07-01 19:21:53 +0200

    INPUT - redesigning the input line format - splitting line for each numeric component

--- uis/inputreader.py
+++ uis/inputreader.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Optional
+from typing import Optional, List
 
 import exiting
 import msgconsumer
@@ -21,30 +21,38 @@ class InputReader(AbstractReader):
 
     def _readMsg(self) -> Optional['Message']:
         line = input("READER ID " + str(self.id) + ": Enter msg:\n")
-        if line.startswith(str(MsgID.SET_VOL.value)):
-            volume = int(line[1:])
+        parts = line.split()  # type: List[str]
+        if len(parts) > 0:
+            if parts[0] == 'E':
+                exiting.exitCleanly(0)
+            elif parts[0].isdigit():
+                msg = self._formatMessage(parts)
+                if msg is not None:
+                    return msg
+
+        logging.warning("Unknown command " + line)
+        return None
+
+    def _formatMessage(self, parts: List[str]) -> Optional['Message']:
+        msgID = int(parts[0])
+        if msgID == MsgID.SET_VOL.value:
+            volume = int(parts[1])
             return IntegerMsg(value=volume, fromID=self.id, typeID=MsgID.SET_VOL, forID=ModuleID.VOLUME_OPERATOR)
 
-        elif line.startswith(str(MsgID.REQ_CURRENT_VOL_INFO.value)):
+        elif msgID == MsgID.REQ_CURRENT_VOL_INFO.value:
             return RequestMsg(self.id, typeID=MsgID.REQ_CURRENT_VOL_INFO, forID=ModuleID.VOLUME_OPERATOR)
+        if msgID == MsgID.SET_SOURCE_STATUS.value:
+            sourceID = int(parts[1])
+            statusID = int(parts[2])
+            return IntegerMsg(value=statusID, fromID=self.id, typeID=MsgID.SET_SOURCE_STATUS,
+                              forID=ModuleID(sourceID))
 
-        if line.startswith(str(MsgID.SET_SOURCE_STATUS.value)):
-            sourceID = int(line[1])
-            statusID = int(line[2])
-            return IntegerMsg(value=statusID, fromID=self.id, typeID=MsgID.SET_SOURCE_STATUS, forID=ModuleID(sourceID))
-
-        elif line.startswith(str(MsgID.REQ_SOURCE_STATUS.value)):
+        elif msgID == MsgID.REQ_SOURCE_STATUS.value:
             return RequestMsg(self.id, typeID=MsgID.REQ_SOURCE_STATUS, groupID=GroupID.SOURCE)
-
-        if line.startswith(str(MsgID.ACTIVATE_SOURCE.value)):
-            sourceID = int(line[1])
+        if msgID == MsgID.ACTIVATE_SOURCE.value:
+            sourceID = int(parts[1])
             return IntegerMsg(value=sourceID, fromID=self.id, typeID=MsgID.ACTIVATE_SOURCE, groupID=GroupID.SOURCE)
-
-        elif line.startswith('E'):
-            exiting.exitCleanly(0)
-
         else:
-            logging.warning("Unknown command " + line)
             return None
 
     def _processMsg(self, msg: 'Message'):
