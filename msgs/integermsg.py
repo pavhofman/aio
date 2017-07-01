6436205e30eb5b2f4fb8cd16ce6623ad8967766b
Pavel Hofman <pavel.hofman@ivitera.com>
2017-07-01 19:30:20 +0200

    MSG - adding BiIntegerMessage

--- msgs/integermsg.py
+++ msgs/integermsg.py
@@ -15,3 +15,18 @@ class IntegerMsg(Message):
 
     def __str__(self) -> str:
         return super().__str__() + "; Value: " + str(self.value)
+
+
+class BiIntegerMsg(Message):
+    """
+    Msg with two int values
+    """
+
+    def __init__(self, value1: int, value2: int, fromID: ModuleID, typeID: MsgID, forID=ModuleID.ANY,
+                 groupID=GroupID.ANY):
+        super().__init__(fromID=fromID, typeID=typeID, forID=forID, groupID=groupID)
+        self.value1 = value1
+        self.value2 = value2
+
+    def __str__(self) -> str:
+        return super().__str__() + "; Value1: " + str(self.value1) + "; Value2: " + str(self.value2)
