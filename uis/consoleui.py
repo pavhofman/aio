ec057f2c505ea61f917f37b2e592788283956f9a
Pavel Hofman <pavel.hofman@ivitera.com>
2017-09-10 15:36:26 +0200

    UI - merging UI and its only ancestor ConsoleUI. Removing the UI class

--- uis/consoleui.py
+++ uis/consoleui.py
@@ -1,9 +1,14 @@
+import logging
 from typing import List, TYPE_CHECKING
 
 from moduleid import ModuleID
+from msgconsumer import MsgConsumer
+from msgid import MsgID
+from msgs.integermsg import IntegerMsg
 from msgs.message import Message
+from sources.sourcestatus import SourceStatus
+from uis.hassourceparts import HasSourceParts
 from uis.sourcepart import SourcePart
-from uis.ui import UI
 
 if TYPE_CHECKING:
     from dispatcher import Dispatcher
@@ -13,16 +18,39 @@ UI
 '''
 
 
-class ConsoleUI(UI):
+class ConsoleUI(MsgConsumer, HasSourceParts):
     # noinspection PyShadowingBuiltins
     def __init__(self, id: ModuleID, dispatcher: 'Dispatcher'):
-        # call the thread class
-        super().__init__(id=id, dispatcher=dispatcher)
+        MsgConsumer.__init__(self, id=id, dispatcher=dispatcher)
+        HasSourceParts.__init__(self)
+        self._volume = 0
 
     # consuming the message
     def _consume(self, msg: 'Message'):
-        super()._consume(msg)
         print(self.__class__.__name__ + " received msg: " + msg.__str__())
+        if msg.typeID == MsgID.CURRENT_VOL_INFO:
+            msg = msg  # type: IntegerMsg
+            self._handleVolMsg(msg)
+        elif msg.typeID == MsgID.SOURCE_STATUS_INFO:
+            msg = msg  # type: IntegerMsg
+            self._handleSourceStatusMsg(msg)
+
+    def _handleVolMsg(self, msg: IntegerMsg):
+        newVolume = msg.value
+        if self._volume != newVolume:
+            self._volume = newVolume
+            logging.debug("Changed volume to: " + str(self._volume))
+
+    def _handleSourceStatusMsg(self, msg: IntegerMsg):
+        source = self._getSourcePartFor(msg)
+        if source is not None:
+            newStatus = SourceStatus(msg.value)
+            if newStatus != source.sourceStatus:
+                source.sourceStatus = newStatus
+                if newStatus.isActivated():
+                    self.activeSource = source
+                logging.debug(
+                    "Changed status of UI Source " + str(source) + " to " + str(source.sourceStatus))
 
     def _initSourceParts(self) -> List['SourcePart']:
         return [
