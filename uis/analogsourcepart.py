5f4b63500c84c7e433261217310762cd2496ed86
Pavel Hofman <pavel.hofman@ivitera.com>
2017-09-10 17:50:25 +0200

    SOURCE UI - removing unused method AnalogSourcePart._fillSelectorBox

--- uis/analogsourcepart.py
+++ uis/analogsourcepart.py
@@ -1,7 +1,6 @@
 from typing import TYPE_CHECKING
 
 from moduleid import ModuleID
-from remi import gui
 from uis.websourcepart import WebSourcePart
 
 if TYPE_CHECKING:
@@ -11,6 +10,3 @@ if TYPE_CHECKING:
 class AnalogSourcePart(WebSourcePart):
     def __init__(self, app: 'WebApp'):
         super().__init__(sourceID=ModuleID.ANALOG_SOURCE, name="Analog", app=app)
-
-    def _fillSelectorBox(self, box: gui.Widget) -> None:
-        pass
