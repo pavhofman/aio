9836ffa38a3655a87d21051d67b6bfb1167bcdb5
Pavel Hofman <pavel.hofman@ivitera.com>
2017-09-24 20:49:46 +0200

    TREE SOURCE - moving general tree methods from MPVTreeSource to TreeSource

--- sources/mpvtreesource.py
+++ sources/mpvtreesource.py
@@ -1,7 +1,7 @@
 import abc
 import re
 from time import sleep
-from typing import TYPE_CHECKING, Optional, Tuple, List, TypeVar, Generic, Dict
+from typing import TYPE_CHECKING, Optional, List, Dict, Generic
 
 from groupid import GroupID
 from metadata import Metadata
@@ -10,24 +10,20 @@ from msgid import MsgID
 from msgs.audioparamsmsg import ParamsItem, AudioParamsMsg
 from msgs.integermsg import BiIntegerMsg
 from msgs.jsonmsg import JsonMsg
-from msgs.nodemsg import NodeMsg, NodeItem, NodeID, NON_EXISTING_NODE_ID, NodeStruct
+from msgs.nodemsg import NON_EXISTING_NODE_ID
 from msgs.trackmsg import TrackMsg, TrackItem
 from sources.metadataparser import MetadataParser
 from sources.playbackstatus import PlaybackStatus
-from sources.treesource import TreeSource, MAX_CHILDREN
+from sources.treesource import TreeSource, PATH
 from sources.usesmpv import UsesMPV
 
 if TYPE_CHECKING:
     from dispatcher import Dispatcher
 
-PATH = TypeVar('PATH')
-
 
 class MPVTreeSource(TreeSource, UsesMPV, Generic[PATH]):
     # noinspection PyShadowingBuiltins
     def __init__(self, id: ModuleID, dispatcher: 'Dispatcher', monitorTime: bool):
-        self._rootNode = None
-        self._playedNodeID = NON_EXISTING_NODE_ID  # type: NodeID
         self._metadataParser = MetadataParser(rules=self._getMetadataParserRules())
         TreeSource.__init__(self, id=id, dispatcher=dispatcher)
         UsesMPV.__init__(self, monitorTime=monitorTime)
@@ -55,94 +51,6 @@ class MPVTreeSource(TreeSource, UsesMPV, Generic[PATH]):
     def close(self):
         TreeSource.close(self)
         UsesMPV.close(self)
-
-    def _sendNodeInfo(self, nodeID: NodeID, fromIndex: int) -> None:
-        nodeID = self._getExistingNodeID(nodeID)
-        path = self._getPath(nodeID)  # type: PATH
-        nodeItem = self._createNodeItem(nodeID, path)
-        totalParents, parentID = self._getParentsTotalAndID(path)
-        children, totalChildren = self._findChildrenAndTotal(path, fromIndex)
-        struct = NodeStruct(node=nodeItem,
-                            rootNode=self._rootNode,
-                            totalParents=totalParents,
-                            parentID=parentID,
-                            children=children,
-                            fromChildIndex=fromIndex,
-                            totalChildren=totalChildren)
-        msg = NodeMsg(nodeStruct=struct, fromID=self.id, groupID=GroupID.UI)
-        self.dispatcher.distribute(msg)
-
-    def _sendParentNodeInfo(self, nodeID: NodeID) -> None:
-        nodeID = self._getExistingNodeID(nodeID)
-        if nodeID == self._rootNode.nodeID:
-            self._sendNodeInfo(self._rootNode.nodeID, 0)
-        else:
-            path = self._getPath(nodeID)
-            parentPath = self._getParentPath(path)
-            index, total = self._findIndexOfPath(path, parentPath)
-            fromIndex = self._calculateFromIndex(index, total)
-            parentID = self._getID(parentPath)
-            self._sendNodeInfo(parentID, fromIndex)
-
-    def _findIndexOfPath(self, path: PATH, parentPath: PATH) -> (int, int):
-        index = 0
-        orderedPaths = self._getOrderedChildPaths(parentPath)
-        for childPath in orderedPaths:
-            if self._areEqual(childPath, path):
-                return index, len(orderedPaths)
-            index += 1
-        return 0, len(orderedPaths)
-
-    def _findChildrenAndTotal(self, path: PATH, fromIndex: int) -> Tuple[List[NodeItem], int]:
-        index = 0
-        total = 0
-        childrenCount = 0
-        children = []  # type: List[NodeItem]
-        if not self._isLeaf(path):
-            # sorted by case insensitive name
-            for childPath in self._getOrderedChildPaths(path):
-                if (index >= fromIndex) and (childrenCount < MAX_CHILDREN):
-                    children.append(self._getNodeItemForPath(childPath))
-                    childrenCount += 1
-                index += 1
-                total += 1
-        return children, total
-
-    def _createNodeItemForID(self, nodeID: NodeID) -> NodeItem:
-        path = self._getPath(nodeID)
-        return self._createNodeItem(nodeID, path)
-
-    def _getNodeItemForPath(self, path: PATH) -> NodeItem:
-        nodeID = self._getID(path)
-        return self._createNodeItem(nodeID, path)
-
-    def _createNodeItem(self, nodeID: NodeID, path: PATH) -> NodeItem:
-        if self._rootNode is not None and nodeID == self._rootNode.nodeID:
-            return self._rootNode
-        else:
-            return NodeItem(nodeID=nodeID, label=self._getNodeLabelFor(path), isPlayable=self._isPlayable(path),
-                            isLeaf=self._isLeaf(path))
-
-    def _getExistingNodeID(self, nodeID: NodeID) -> NodeID:
-        path = self._getPath(nodeID)
-        if path is None:
-            return self._rootNode.nodeID
-        else:
-            return nodeID
-
-    def _getParentsTotalAndID(self, path: PATH) -> Tuple[int, NodeID]:
-        rootPath = self._getRootPath()  # type: PATH
-        if path.__eq__(rootPath):
-            # directly _xmlRoot
-            return 0, NON_EXISTING_NODE_ID
-        else:
-            parentPath = self._getParentPath(path)  # type: PATH
-            directParentID = self._getID(parentPath)
-            totalParents = 1  # type: int
-            while not parentPath.__eq__(rootPath):
-                parentPath = self._getParentPath(parentPath)
-                totalParents += 1
-            return totalParents, directParentID
 
     def chapterWasChanged(self, chapter: int):
         pass
@@ -205,54 +113,6 @@ class MPVTreeSource(TreeSource, UsesMPV, Generic[PATH]):
             path = self._getPathFor(mpvPath)
             if path is not None and self._isPlayable(path):
                 self._switchedToNewPath(path)
-
-    @abc.abstractmethod
-    def _getPathFor(self, filePath: str) -> Optional[PATH]:
-        pass
-
-    @abc.abstractmethod
-    def _getRootPath(self) -> PATH:
-        pass
-
-    @abc.abstractmethod
-    def _getNodeLabelFor(self, path: PATH) -> str:
-        pass
-
-    @abc.abstractmethod
-    def _getTrackLabelFor(self, path: PATH) -> str:
-        pass
-
-    @abc.abstractmethod
-    def _getID(self, path: PATH) -> NodeID:
-        pass
-
-    @abc.abstractmethod
-    def _getPath(self, nodeID: NodeID) -> Optional[PATH]:
-        pass
-
-    @abc.abstractmethod
-    def _getRootNodeItem(self) -> NodeItem:
-        pass
-
-    @abc.abstractmethod
-    def _getParentPath(self, path: PATH) -> PATH:
-        pass
-
-    @abc.abstractmethod
-    def _isLeaf(self, path: PATH) -> bool:
-        pass
-
-    @abc.abstractmethod
-    def _isPlayable(self, path: PATH) -> bool:
-        pass
-
-    @abc.abstractmethod
-    def _getOrderedChildPaths(self, path: PATH) -> List[PATH]:
-        pass
-
-    @abc.abstractmethod
-    def _areEqual(self, path1: PATH, path2: PATH) -> bool:
-        pass
 
     @abc.abstractmethod
     def _getMetadataParserRules(self) -> Dict[Metadata, List[str]]:
--- sources/treesource.py
+++ sources/treesource.py
@@ -1,11 +1,12 @@
 import abc
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TypeVar, List, Optional, Generic, Tuple
 
+from groupid import GroupID
 from moduleid import ModuleID
 from msgid import MsgID
 from msgs.integermsg import BiIntegerMsg, IntegerMsg
 from msgs.message import Message
-from msgs.nodemsg import NodeID
+from msgs.nodemsg import NodeID, NON_EXISTING_NODE_ID, NodeItem, NodeStruct, NodeMsg
 from sources.source import Source
 
 if TYPE_CHECKING:
@@ -16,14 +17,18 @@ MAX_CHILDREN = 5
 # how many singling nodes will be displayed before current node in parent request
 PREVIOUS_SIBLINGS = 2
 
+PATH = TypeVar('PATH')
 
-class TreeSource(Source, abc.ABC):
+
+class TreeSource(Source, abc.ABC, Generic[PATH]):
     """
     Source representation. Only one instance for each source within the network
     """
 
     # noinspection PyShadowingBuiltins
     def __init__(self, id: ModuleID, dispatcher: 'Dispatcher'):
+        self._rootNode = None
+        self._playedNodeID = NON_EXISTING_NODE_ID  # type: NodeID
         # call the thread class
         super().__init__(id=id, dispatcher=dispatcher)
 
@@ -45,7 +50,96 @@ class TreeSource(Source, abc.ABC):
         else:
             return False
 
-    def _calculateFromIndex(self, nodeIndex: int, total: int) -> int:
+    def _sendNodeInfo(self, nodeID: NodeID, fromIndex: int) -> None:
+        nodeID = self._getExistingNodeID(nodeID)
+        path = self._getPath(nodeID)  # type: PATH
+        nodeItem = self._createNodeItem(nodeID, path)
+        totalParents, parentID = self._getParentsTotalAndID(path)
+        children, totalChildren = self._findChildrenAndTotal(path, fromIndex)
+        struct = NodeStruct(node=nodeItem,
+                            rootNode=self._rootNode,
+                            totalParents=totalParents,
+                            parentID=parentID,
+                            children=children,
+                            fromChildIndex=fromIndex,
+                            totalChildren=totalChildren)
+        msg = NodeMsg(nodeStruct=struct, fromID=self.id, groupID=GroupID.UI)
+        self.dispatcher.distribute(msg)
+
+    def _sendParentNodeInfo(self, nodeID: NodeID) -> None:
+        nodeID = self._getExistingNodeID(nodeID)
+        if nodeID == self._rootNode.nodeID:
+            self._sendNodeInfo(self._rootNode.nodeID, 0)
+        else:
+            path = self._getPath(nodeID)
+            parentPath = self._getParentPath(path)
+            index, total = self._findIndexOfPath(path, parentPath)
+            fromIndex = self._calculateFromIndex(index, total)
+            parentID = self._getID(parentPath)
+            self._sendNodeInfo(parentID, fromIndex)
+
+    def _findIndexOfPath(self, path: PATH, parentPath: PATH) -> (int, int):
+        index = 0
+        orderedPaths = self._getOrderedChildPaths(parentPath)
+        for childPath in orderedPaths:
+            if self._areEqual(childPath, path):
+                return index, len(orderedPaths)
+            index += 1
+        return 0, len(orderedPaths)
+
+    def _findChildrenAndTotal(self, path: PATH, fromIndex: int) -> Tuple[List[NodeItem], int]:
+        index = 0
+        total = 0
+        childrenCount = 0
+        children = []  # type: List[NodeItem]
+        if not self._isLeaf(path):
+            # sorted by case insensitive name
+            for childPath in self._getOrderedChildPaths(path):
+                if (index >= fromIndex) and (childrenCount < MAX_CHILDREN):
+                    children.append(self._getNodeItemForPath(childPath))
+                    childrenCount += 1
+                index += 1
+                total += 1
+        return children, total
+
+    def _createNodeItemForID(self, nodeID: NodeID) -> NodeItem:
+        path = self._getPath(nodeID)
+        return self._createNodeItem(nodeID, path)
+
+    def _getNodeItemForPath(self, path: PATH) -> NodeItem:
+        nodeID = self._getID(path)
+        return self._createNodeItem(nodeID, path)
+
+    def _createNodeItem(self, nodeID: NodeID, path: PATH) -> NodeItem:
+        if self._rootNode is not None and nodeID == self._rootNode.nodeID:
+            return self._rootNode
+        else:
+            return NodeItem(nodeID=nodeID, label=self._getNodeLabelFor(path), isPlayable=self._isPlayable(path),
+                            isLeaf=self._isLeaf(path))
+
+    def _getExistingNodeID(self, nodeID: NodeID) -> NodeID:
+        path = self._getPath(nodeID)
+        if path is None:
+            return self._rootNode.nodeID
+        else:
+            return nodeID
+
+    def _getParentsTotalAndID(self, path: PATH) -> Tuple[int, NodeID]:
+        rootPath = self._getRootPath()  # type: PATH
+        if path.__eq__(rootPath):
+            # directly _xmlRoot
+            return 0, NON_EXISTING_NODE_ID
+        else:
+            parentPath = self._getParentPath(path)  # type: PATH
+            directParentID = self._getID(parentPath)
+            totalParents = 1  # type: int
+            while not parentPath.__eq__(rootPath):
+                parentPath = self._getParentPath(parentPath)
+                totalParents += 1
+            return totalParents, directParentID
+
+    @staticmethod
+    def _calculateFromIndex(nodeIndex: int, total: int) -> int:
         """
         :param nodeIndex: index of same-level node
         :param total: total of nodes on that level
@@ -61,13 +155,53 @@ class TreeSource(Source, abc.ABC):
         return fromIndex if fromIndex >= 0 else 0
 
     @abc.abstractmethod
-    def _sendNodeInfo(self, nodeID: NodeID, fromIndex: int) -> None:
-        pass
-
-    @abc.abstractmethod
-    def _sendParentNodeInfo(self, nodeID: NodeID) -> None:
-        pass
-
-    @abc.abstractmethod
     def _playNode(self, nodeID: NodeID) -> None:
+        pass
+
+    @abc.abstractmethod
+    def _getPathFor(self, filePath: str) -> Optional[PATH]:
+        pass
+
+    @abc.abstractmethod
+    def _getRootPath(self) -> PATH:
+        pass
+
+    @abc.abstractmethod
+    def _getNodeLabelFor(self, path: PATH) -> str:
+        pass
+
+    @abc.abstractmethod
+    def _getTrackLabelFor(self, path: PATH) -> str:
+        pass
+
+    @abc.abstractmethod
+    def _getID(self, path: PATH) -> NodeID:
+        pass
+
+    @abc.abstractmethod
+    def _getPath(self, nodeID: NodeID) -> Optional[PATH]:
+        pass
+
+    @abc.abstractmethod
+    def _getRootNodeItem(self) -> NodeItem:
+        pass
+
+    @abc.abstractmethod
+    def _getParentPath(self, path: PATH) -> PATH:
+        pass
+
+    @abc.abstractmethod
+    def _isLeaf(self, path: PATH) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def _isPlayable(self, path: PATH) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def _getOrderedChildPaths(self, path: PATH) -> List[PATH]:
+        pass
+
+    @abc.abstractmethod
+    def _areEqual(self, path1: PATH, path2: PATH) -> bool:
         pass
