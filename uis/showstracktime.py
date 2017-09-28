00ce6cd7867a33467928c16b8338fd25f3c1cd5c
Pavel Hofman <pavel.hofman@ivitera.com>
2017-09-28 20:16:13 +0200

    WEB UI - showing converted secsToTime in ShowTracksTime

--- uis/showstracktime.py
+++ uis/showstracktime.py
@@ -1,5 +1,6 @@
 import abc
 
+from common.timeutils import secsToTime
 from remi import gui
 from uis.canappendwidget import CanAppendWidget
 
@@ -16,8 +17,8 @@ class ShowsTrackTime(CanAppendWidget, abc.ABC):
         self.append(self._trackDuration, "3")
 
     def drawTimePos(self, timePos: int, duration: int) -> None:
-        self._timePosLabel.set_text(str(timePos))
-        self._trackDuration.set_text(str(duration))
+        self._timePosLabel.set_text(secsToTime(timePos))
+        self._trackDuration.set_text(secsToTime(duration))
 
     def _clearTrackInfo(self):
         self._timePosLabel.set_text("")
