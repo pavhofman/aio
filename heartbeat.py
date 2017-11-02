ca72dd3494128ce1e6d26fc55484d81dceb2efef
Pavel Hofman <pavel.hofman@ivitera.com>
2017-11-02 16:48:38 +0100

    HEARTBEAT - autorouting requires all attached modules be MsgConsumers - modifying Heartbeat accordingly

--- heartbeat.py
+++ heartbeat.py
@@ -1,45 +1,37 @@
 import time
-from threading import Thread, Event
 from typing import TYPE_CHECKING
 
 from groupid import GroupID
 from moduleid import ModuleID
+from msgconsumer import MsgConsumer
 from msgid import MsgID
+from msgs.message import Message
 from msgs.requestmsg import RequestMsg
 
 if TYPE_CHECKING:
     from dispatcher import Dispatcher
+
 """
 Heartbeat thread periodically requesting status updates 
 """
 
 
-class Heartbeat(Thread):
-    # noinspection PyShadowingNames
+class Heartbeat(MsgConsumer):
     def __init__(self, dispatcher: 'Dispatcher'):
         # call the thread class
-        super().__init__()
-        self.dispatcher = dispatcher
-        self.__event = Event()
-        self.setDaemon(True)
-        # unique ID among modules of same type
-        self.start()
+        super().__init__(id=ModuleID.HEARTBEAT, dispatcher=dispatcher)
 
-    def stop(self):
-        self.__event.set()
-
-    def stopped(self) -> bool:
-        return self.__event.isSet()
-
+    # noinspection PyShadowingNames
     def run(self):
         while not self.stopped():
             time.sleep(2)
             msg = RequestMsg(ModuleID.HEARTBEAT, typeID=MsgID.REQ_SOURCE_STATUS, groupID=GroupID.SOURCE)
-            self.dispatcher.distribute(msg, ModuleID.HEARTBEAT)
+            self.dispatcher.distribute(msg, self.id)
             msg = RequestMsg(ModuleID.HEARTBEAT, typeID=MsgID.REQ_CURRENT_VOL_INFO, forID=ModuleID.VOLUME_OPERATOR)
-            self.dispatcher.distribute(msg, ModuleID.HEARTBEAT)
+            self.dispatcher.distribute(msg, self.id)
             # only one run during development
             self.stop()
 
-    def close(self):
-        self.stop()
+    def _consume(self, msg: 'Message') -> bool:
+        # never called
+        pass
